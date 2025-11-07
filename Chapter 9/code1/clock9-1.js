9bf041ad86f4cc9d522938030610a44c6edf0cbb
Sean Briggs <briggs.sea0642@stu.stech.edu>
2025-11-07 15:03:32 -0700

    Chapter 9 Tutorial and Coding Challenge 9-1

---
+++ Chapter 9/code1/clock9-1.js
@@ -0,0 +1,34 @@
+﻿"use strict"
+
+/*
+	New Perspectives on HTML5 and CSS3, 8th Edition
+	Tutorial 9
+	Coding Challenge 1
+
+	Clock
+	Author: Sean Briggs
+	Date:   2025-11-07
+
+	function getWeekday(dayNum)
+		Returns the text of the day of the week where dayNum
+		is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock() {
+    var thisDay = new Date();
+    var thisDate = thisDay.toLocaleDateString();
+    var thisDayNum = thisDay.getDay();
+    var thisWeekday = getWeekday(thisDayNum);
+    var thisTime = thisDay.toLocaleTimeString();
+    document.getElementById("date").textContent = thisDate;
+    document.getElementById("wday").textContent = thisWeekday;
+    document.getElementById("time").textContent = thisTime;
+}
+
+function getWeekday(dayNum) {
+	var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	return wDays[dayNum];
+}
